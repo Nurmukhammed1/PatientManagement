4b02001b1655883ab1dddace92450b6fcfe5b8d5
nurmukhammedkanafin <nurmuhammed.kanafin@gmail.com>
2025-08-23 22:42:57 +0500

    Initial commit

---
+++ patient-service/src/main/java/com/pm/patientservice/dto/PatientRequestDTO.java
@@ -0,0 +1,64 @@
+package com.pm.patientservice.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class PatientRequestDTO {
+    @NotBlank
+    @Size(max = 100, message = "Name cannot exceed 100 characters")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Adress is required")
+    private String adress;
+
+    @NotBlank(message = "Data of birth is required")
+    private String dataOfBirth;
+
+    @NotBlank(message = "Registered date is required")
+    private String registeredDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getDataOfBirth() {
+        return dataOfBirth;
+    }
+
+    public void setDataOfBirth(String dataOfBirth) {
+        this.dataOfBirth = dataOfBirth;
+    }
+
+    public String getRegisteredDate() {
+        return registeredDate;
+    }
+
+    public void setRegisteredDate(String registeredDate) {
+        this.registeredDate = registeredDate;
+    }
+}
